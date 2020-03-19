a736ee638c3ce582a0dbd8b6656cebcc08caffce
Youuuu2 <sariyounes.dev@gmail.com>
2020-03-19 18:20:31 +0100

    g

--- src/Migrations/Version20200319171017.php
+++ src/Migrations/Version20200319171017.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200319161720 extends AbstractMigration
+final class Version20200319171017 extends AbstractMigration
 {
     public function getDescription() : string
     {
